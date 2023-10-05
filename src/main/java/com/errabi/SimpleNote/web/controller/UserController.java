44f38d519e4e58eb328cd043f3dacb752f346290
errabi ayoub <errabi.ayoub@gmail.com>
2023-10-05 23:14:20 +0100

    Update user controller

--- src/main/java/com/errabi/SimpleNote/web/controller/UserController.java
+++ src/main/java/com/errabi/SimpleNote/web/controller/UserController.java
@@ -3,15 +3,22 @@ package com.errabi.SimpleNote.web.controller;
 import com.errabi.SimpleNote.services.UserService;
 import com.errabi.SimpleNote.web.model.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/simple-note/v1/")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-    @GetMapping("/User/{id}")
-    UserDto getUser(@PathVariable Long id) {
-        return userService.findById(id);
+    @GetMapping("/users/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
+        return new ResponseEntity(userService.findById(id), HttpStatus.OK);
+    }
+    @PostMapping("/users")
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+         return new ResponseEntity<>( userService.save(userDto),HttpStatus.CREATED);
     }
 }
 
